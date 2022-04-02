12fc7f165982f0adf5cc83293e59fcda33a65989
Gangaraj123 <96531549+Gangaraj123@users.noreply.github.com>
2022-04-02 15:07:07 +0530

    Queue implementation

---
+++ Process_Queue.c
@@ -0,0 +1,66 @@
+#include "Process_control_block.h"
+#include <stdlib.h>
+#include<signal.h>
+// function to start a process
+PcbPtr start_pcb(PcbPtr process)
+{
+    // TODO : start a process
+}
+
+PcbPtr Terminate_Pcb(PcbPtr process)
+{
+    
+    if(kill(process->pid,SIGINT))
+        return NULL;
+    return process;
+}
+
+PcbPtr Create_Pcb()
+{
+    PcbPtr temp=(PcbPtr)malloc(sizeof(PCB));
+    return temp;
+}
+
+PcbPtr enqueuePcb(PcbPtr head, PcbPtr process) // returns new head
+{
+    if (head == NULL) // if head is null, make the process as head
+    {
+        head = process;
+        return head;
+    }
+
+    // else find the tail and add process to it
+    PcbPtr temp = head;
+    while (temp != NULL)
+    {
+        if (temp->next == NULL)
+            break;
+        temp = temp->next;
+    }
+    temp->next = process;
+    temp = NULL;
+    return head;
+}
+
+PcbPtr dequeuePcb(PcbPtr head) // returns process dequeued
+{
+    if (head == NULL)
+        return NULL;
+    PcbPtr temp = head;
+    head = head->next;                      
+    temp->next = NULL;
+    return temp; 
+}
+
+int queue_size(PcbPtr head)
+{
+    PcbPtr temp=head;
+    int size=0;
+    while(temp)
+    {
+        size++;
+        temp=temp->next;
+    }
+    temp=NULL;
+    return size;
+}
---
+++ main.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<Process_control_block.h>
+#include<signal.h>
+
+int main(int argc, char const *argv[])
+{
+    PcbPtr process_queue_head=NULL,current_process=NULL;
+    int currently_running_any_process = 0; 
+    // 0 if not, 1 if anyone process is running
+
+    int dispatcher_timer = 0;
+
+    while(currently_running_any_process || (process_queue_head != NULL)) {
+        if(currently_running_any_process) { // any process is running
+            if(current_process->remaining_time == 0){
+                // send SIGINT signal
+                if(kill(current_process->pid,SIGINT))
+                    fprintf(stderr,"terminate of %d failed",(int)current_process->pid);
+                else
+                    printf("Process %s finished executing\n",current_process->args[0]);
+                // Free up memory
+            } else { // Process is still running
+                printf("Currently running process : %s\n",current_process->args[0]);
+                current_process->remaining_time--;
+                // decrease remainingcputime
+            }
+        } else { // No process is running but process queue is not empty
+            if(dispatcher_timer >= process_queue_head->arrival_time) {
+                current_process = dequeuePcb(process_queue_head);
+                // Fork and exec to run process
+                int process_id = fork();
+                if(fork() == 0) { // In the child process
+                    currently_running_any_process=1;
+                    execvp(current_process->args[0], current_process->args);
+                } else { // In the parent process
+
+                }
+            }
+        }
+
+        sleep(1);
+        ++dispatcher_timer;
+    }
+    return 0;
+}
