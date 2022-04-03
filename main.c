e56c78a5b119b0c774930fbabdd579908aab3603
Chintan Suthar <75067919+chintansuthar72@users.noreply.github.com>
2022-04-03 15:03:56 +0530

    Update main.c

--- main.c
+++ main.c
@@ -5,8 +5,7 @@
 int main(int argc, const char *argv[])
 {
     PcbPtr process_queue_head=NULL,current_process=NULL;
-    int currently_running_any_process = 0; 
-    // 0 if not, 1 if anyone process is running
+    int currently_running_any_process = 0;  // 0 if not, 1 if anyone process is running
 
     int dispatcher_timer = 0;
 
@@ -19,21 +18,21 @@ int main(int argc, const char *argv[])
                 else
                     printf("Process %s finished executing\n",current_process->args[0]);
                 // Free up memory
+                currently_running_any_process = 0;
             } else { // Process is still running
                 printf("Currently running process : %s\n",current_process->args[0]);
-                current_process->remaining_time--;
-                // decrease remainingcputime
+                current_process->remaining_time--; // decrease remainingcputime
             }
         } else { // No process is running but process queue is not empty
             if(dispatcher_timer >= process_queue_head->arrival_time) {
                 current_process = dequeuePcb(process_queue_head);
                 // Fork and exec to run process
                 int process_id = fork();
-                if(fork() == 0) { // In the child process
-                    currently_running_any_process=1;
+                if(process_id == 0) { // In the child process
+                    currently_running_any_process = 1;
                     execvp(current_process->args[0], current_process->args);
                 } else { // In the parent process
-
+                    // Do nothing
                 }
             }
         }
