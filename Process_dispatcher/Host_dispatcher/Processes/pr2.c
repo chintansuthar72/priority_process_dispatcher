483720ce06bfb9feebb296dcc3782db14572dd4d
Gangaraj123 <96531549+Gangaraj123@users.noreply.github.com>
2022-04-26 09:45:13 +0530

    Delete Process_dispatcher/Host_dispatcher directory

--- Process_dispatcher/Host_dispatcher/Processes/pr2.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<unistd.h>
-int main(){
-	int n = 100 ;
-	printf("%d\n",getpid());
-	sleep(5);
-	printf("\nTesting Process 2\n") ;
-	for(int i=0;i<n;i++) printf("%d,",i); 
-	}
--- Process_dispatcher/Host_dispatcher/Processes/pr4.c
+++
@@ -1,5 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	printf("\nTesting process pr4.\nHello World Hello Make Hello Everyone Hello People\n\n") ;
-}
